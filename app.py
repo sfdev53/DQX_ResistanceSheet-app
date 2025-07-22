04be2c1afcd13fe5963ad2cb0b9243bd42eec4c3
sfdev53 <sfdev53@gmail.com>
2025-07-22 14:24:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'sitemap.xml')
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('static', 'robots.txt')
 
 if __name__ == '__main__':
     app.run(debug=True)
